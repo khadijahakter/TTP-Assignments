1b727355e230b81b897b269eacabff9f2df9fb32
Khadijah Akter <khadijahakter123@gmail.com>
2023-06-29 13:58:32 -0400

    added a review button to each card

--- React-Assignment/Book-Review-Forum/src/bookCard.jsx
+++ React-Assignment/Book-Review-Forum/src/bookCard.jsx
@@ -1,19 +1,19 @@
-export default function BookCard({book}) {
-    const { 
-        image: { src, alt }, 
-        title, 
-        author, 
-        pages, 
-        genres, 
-        publishDate 
+export default function BookCard({ book, showModal }) {
+    const {
+        image: { src, alt },
+        title,
+        author,
+        pages,
+        genres,
+        publishDate
     } = book;
 
     return (
-        <div className = "b-desc">
+        <div className="b-desc">
             <img
                 className="b-desc__book-image"
-                src = {src}
-                alt = {alt}
+                src={src}
+                alt={alt}
             />
             <div className="b-desc__details">
                 <h2 className="b-desc__book-title">{title}</h2>
@@ -23,6 +23,15 @@ export default function BookCard({book}) {
                     <li className="b-desc__genres">{genres}</li>
                     <li className="b-desc__publish-date">{publishDate}</li>
                 </ul>
+                <div className="flex justify-between">
+                    <div>
+                        <button
+                            className="bg-gray-200 px-4 py-2 hover:bg-gray-300 transition m-3"
+                            onClick={showModal}
+                        >Submit Review
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
