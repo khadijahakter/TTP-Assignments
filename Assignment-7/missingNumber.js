36d3333d06ab37654c7624a4504e7a127b8a982a
Khadijah Akter <khadijahakter123@gmail.com>
2023-07-10 02:03:03 -0400

    added psuedocode

--- Assignment-7/missingNumber.js
+++ Assignment-7/missingNumber.js
@@ -1,22 +1,22 @@
-// set A question 1
-function missingNumber(nums)
-{
-  let i = 0
-        while (i < nums.length) {
-            let correctIndex = nums[i]
-            if (nums[i] < nums.length && nums[i] != nums[correctIndex])
-             [nums[i],nums[correctIndex]]= [nums[correctIndex],nums[i]]
-            else
-             i++
+function missingNumber(nums) {
+    let i = 0
+    while (i < nums.length) {
+        let correctIndex = nums[i] // cyclic sort
+        if (nums[i] < nums.length && nums[i] != nums[correctIndex])
+            [nums[i], nums[correctIndex]] = [nums[correctIndex], nums[i]];
+        else
+            i++;
+    }
+    // search for 1st missing number
+    for (let j = 0; j < nums.length; j++) {
+        if (nums[j] != j) {
+            return j;
         }
-        //Search for 1st missing number
-        for (let j = 0; j < nums.length; j++) {
-            if (nums[j] != j) {
-                return j
-            }
-        }
-        return nums.length;
+    }
+    return nums.length;
 }
 
-console.log(missingNumber([0,1]));  
-console.log(missingNumber([9,6,4,2,3,5,7,0,1]));
+console.log(missingNumber([0, 1]));
+console.log(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1]));
+
+module.exports = missingNumber;
