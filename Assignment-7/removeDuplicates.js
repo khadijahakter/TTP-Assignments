af5918c53e5dc757202666331fda914bb9c1ab20
Khadijah Akter <khadijahakter123@gmail.com>
2023-07-10 11:54:52 -0400

    added pseudocode

--- Assignment-7/removeDuplicates.js
+++ Assignment-7/removeDuplicates.js
@@ -1,25 +1,26 @@
-// set B question 1
-var removeDuplicates = function(nums) {
-    let i = 0;
-    let j = 0;
-    while(j < nums.length)
-      {
-        if(nums[i]==nums[j])
-        {
-          j++
-        }
-        else{
-          i++;
-          nums[i] = nums[j];
-        }
-         
-      }
-     i++;
-     nums.length = i;
-     return nums;
- };
+var removeDuplicates = function (nums) {
+  let i = 0; // pointer for the current unique element
+  let j = 0; // pointer for iterating through the array
+  while (j < nums.length) { 
+    if (nums[i] == nums[j]) { 
+      j++; // move the j pointer if the current element is a duplicate
+    }
+    else {
+      i++;  // move the i pointer to the next unique element
+      nums[i] = nums[j]; // replace the element at i with the unique element at j
+    }
+
+  }
+
+  i++; // increment i to include the last unique element
+  nums.length = i; // set the length of the array to remove any remaining duplicate elements
+  return nums;
+
+};
 
 nums = [1, 1, 1, 1, 2, 3, 4, 4, 5]
 nums1 = [20, 20, 40]
 console.log(removeDuplicates(nums));
 console.log(removeDuplicates(nums1));
+
+module.exports = removeDuplicates;
