29f4ec195fbbf05b504dfc934a4cf58e85118edd
Khadijah Akter <khadijahakter123@gmail.com>
2023-07-10 02:00:16 -0400

    added psuedocode

--- Assignment-7/findDuplicates.js
+++ Assignment-7/findDuplicates.js
@@ -1,17 +1,16 @@
-// set A question 2
 function findDuplicates(nums) {
 
-    let duplicates = new Set();
+    let duplicates = new Set(); // let duplicates be a set
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] == nums[j]) {
-                duplicates.add(nums[i]);
+            if (nums[i] == nums[j]) { // duplicate found
+                duplicates.add(nums[i]); // add duplicate to the set
             }
         }
 
     }
-    let arr = Array.from(duplicates);
-    return arr;
+    let arr = Array.from(duplicates); // make the set into a array
+    return arr; // return duplicates
 }
 
 nums = [10, 5, 2, 2, 4, 3, 4, 6, 7, 8, 9, 101, 20, 101]
@@ -19,19 +18,18 @@ nums1 = [102, 100, 204, 100, 100, 102]
 console.log(findDuplicates(nums));
 console.log(findDuplicates(nums1));
 
+module.exports = findDuplicates;
+
 /*
-Input: nums = [4,3,2,7,8,2,3,1]
-Output: [2,3]
+nums = [4,3,2,7,8,2,3,1]
+[2,3]
 
-Input: nums = [1,1,2]
-Output: [1]
+nums = [1,1,2]
+[1]
 
-Input: nums = [1]
-Output: []
-
-    steps:
-    sort the array
-    add duplicate value to new array to return
-    but cyclic sort returns undefined, so possibly break up into sub array?
-
+cyclic sort does not mutate the original array and sorts in place
+in this solution an array is created to track duplicates, since we had troubke
+returning the array at first we used a set and added duplicates to the set
+with two for loops where i is assigned the index value and j is assigned index + 1
+the set is turned back into an array and duplicate values are returned
 */
