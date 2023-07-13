e274bd7bd86731ad5bb80ede653fce9ae98ba5cf
Khadijah Akter <52975594+khadillacs@users.noreply.github.com>
2023-07-12 23:37:41 -0400

    Delete cyclic-sort-fast-and-slow-pointers directory

--- cyclic-sort-fast-and-slow-pointers/missingNumber.test.js
+++
@@ -1,8 +0,0 @@
-const missingNumber = require('./missingNumber'); 
-
-describe('Find the Missing Number', () => {
-  it('should return the missing number in the given array', () => {
-    expect(missingNumber([0, 1])).toEqual(2);
-    expect(missingNumber([9, 6, 4, 2, 3, 5, 7, 0, 1])).toEqual(8);
-  });
-});
