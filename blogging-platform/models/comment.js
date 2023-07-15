dc48d1c0204443931aa0c89469c3e782b7fe4f5a
Khadijah Akter <khadijahakter123@gmail.com>
2023-07-15 01:40:07 -0400

    database setup and relationships

---
+++ blogging-platform/models/comment.js
@@ -0,0 +1,19 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    static associate(models) {
+      this.belongsTo(User);
+      this.belongsTo(Post);
+    }
+  }
+  Comment.init({
+    content: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Comment',
+  });
+  return Comment;
+};
---
+++ blogging-platform/models/post.js
@@ -0,0 +1,18 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Post extends Model {
+    static associate(models) {
+      this.hasMany(Comment);
+    }
+  }
+  Post.init({
+    content: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Post',
+  });
+  return Post;
+};
