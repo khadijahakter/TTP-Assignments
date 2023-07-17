c6954d5fd6adede622cf95fd5dbf49f257541a0f
Khadijah Akter <khadijahakter123@gmail.com>
2023-07-17 03:12:58 -0400

    implemented crud operations for posts

--- blogging-platform/server.js
+++ blogging-platform/server.js
@@ -40,6 +40,119 @@ app.post("/signup", async (req, res) => {
     }
 })
 
+// Get all the posts
+app.get("/posts", authenticateUser, async (req, res) => {
+    try {
+      const allPosts = await Post.findAll();
+  
+      res.status(200).json(allPosts);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: err.message });
+    }
+  });
+
+// Get a specific post
+app.get("/posts/:id", authenticateUser, async (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+  
+    try {
+      const post = await Post.findOne({ where: { id: postId } });
+  
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res.status(404).send({ message: "Post not found." });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: err.message });
+    }
+  });
+
+// Create a new post
+app.post("/posts", authenticateUser, async (req, res) => {
+    try {
+      const newPost = await Post.create(req.body);
+  
+      res.status(201).json(newPost);
+    } catch (err) {
+      if (err.name === "SequelizeValidationError") {
+        return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+      }
+      console.error(err);
+      res.status(500).send({ message: err.message });
+    }
+  });
+
+  // Update a specific post
+  app.patch("/posts/:id", authenticateUser, async (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+  
+    try {
+      const record = await Post.findOne({ where: { id: postId } });
+      if (record && record.UserId !== parseInt(req.session.userId, 10)) {
+        return res
+          .status(403)
+          .json({ message: "You are not authorized to perform that action." });
+      }
+      const [numberOfAffectedRows, affectedRows] = await Post.update(
+        req.body,
+        { where: { id: postId }, returning: true }
+      );
+  
+      if (numberOfAffectedRows > 0) {
+        res.status(200).json(affectedRows[0]);
+      } else {
+        res.status(404).send({ message: "Post not found." });
+      }
+    } catch (err) {
+      if (err.name === "SequelizeValidationError") {
+        return res.status(422).json({ errors: err.errors.map((e) => e.message) });
+      }
+      console.error(err);
+      res.status(500).send({ message: err.message });
+    }
+  });
+
+  // Delete a specific post
+app.delete("/postspost/:id", authenticateUser, async (req, res) => {
+    const postId = parseInt(req.params.id, 10);
+  
+    try {
+      const record = await Post.findOne({ where: { id: postId } });
+      if (record && record.UserId !== parseInt(req.session.userId, 10)) {
+        return res
+          .status(403)
+          .json({ message: "You are not authorized to perform that action." });
+      }
+      const deleteOp = await Post.destroy({ where: { id: postId } });
+  
+      if (deleteOp > 0) {
+        res.status(200).send({ message: "Post deleted successfully" });
+      } else {
+        res.status(404).send({ message: "Post not found" });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).send({ message: err.message });
+    }
+  });
+
+/*
+Task 5: CRUD Operations for Comments 
+
+Implement CRUD operations for the Comments resource.
+Create API endpoints to handle the following operations:
+Create a new Comment.
+Retrieve all Comments for a specific Post.
+Retrieve a specific Comment by ID.
+Update a Comment.
+Delete a Comment.
+Ensure that only authenticated users can perform CRUD operations on Comments.
+Test the CRUD operations using Postman.
+*/
+
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
